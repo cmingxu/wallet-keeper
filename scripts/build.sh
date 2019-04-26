861fb1ed4efafb5e31cdfee99c235d69c71c94c0
kevin xu <cming.xu@gmail.com>
2019-04-26 22:30:33 +0800

    should set proxy if golang.org not reachable
    Signed-off-by: kevin xu <cming.xu@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,9 @@
 
 ROOT_PATH=$(cd "$(dirname $BASH_SOURCE[0])/.." && pwd)
 
+http_proxy=${http_proxy:-socks5://192.168.0.102:1080}
+https_proxy=${https_proxy:-socks5://192.168.0.102:1080}
+
 BUILD_CONTAINER_NAME=wallet_keeper_build
 BUILD_IMAGE=wk_build_base
 
@@ -13,7 +16,21 @@ if [[ -z $(docker images | grep "$BUILD_IMAGE") ]]; then
   docker build -t $BUILD_IMAGE --no-cache --rm -f ./Dockerfile.build .
 fi
 
-docker run --rm --name $BUILD_CONTAINER_NAME -v "$(pwd)":/go/src/app $BUILD_IMAGE make
+# check if golang.org can be reached
+ping -q -W 1 -c 1 golang.org
+if [ $? == "0" ]; then
+  ENV=''
+else
+  ENV="--env http_proxy=${http_proxy} --env https_proxy=${https_proxy}"
+fi
+
+
+echo ${ENV}
+docker run --rm \
+  --name $BUILD_CONTAINER_NAME \
+  -v "$(pwd)":/go/src/app \
+  ${ENV} \
+  $BUILD_IMAGE make
 
 binary=./bin/wallet-keeper-${VERSION}
 if [[ -f ${binary} ]]; then
