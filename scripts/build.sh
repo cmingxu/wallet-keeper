f4ce7974d874e4e518a4abbcf863bc25bea0728d
Deshi Xiao <xiaods@gmail.com>
2019-05-02 08:35:11 +0800

    use goproxy to bypass GFW block golang.org way

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,8 +2,40 @@
 
 ROOT_PATH=$(cd "$(dirname $BASH_SOURCE[0])/.." && pwd)
 
+GOPROXY=${GOPROXY:-https://goproxy.io}
+
+BUILD_CONTAINER_NAME=wallet_keeper_build
+BUILD_IMAGE=wk_build_base
+
+cd $ROOT_PATH
 VERSION=$(cat ./VERSION)
 RELEASE_IMAGE=wallet_keeper:${VERSION}
 
+if [[ -z $(docker images | grep "$BUILD_IMAGE") ]]; then
+  docker build -t $BUILD_IMAGE --no-cache --rm -f ./Dockerfile.build .
+fi
+
+# check if golang.org can be reached
+ping -q -W 1 -c 1 golang.org
+if [ $? == "0" ]; then
+  ENV=''
+else
+  ENV="--env GOPROXY=${GOPROXY}"
+fi
+
+
+echo ${ENV}
+docker run --rm \
+  --name $BUILD_CONTAINER_NAME \
+  -v "$(pwd)":/go/src/app \
+  ${ENV} \
+  $BUILD_IMAGE make
+
+binary=./bin/wallet-keeper-${VERSION}
+if [[ -f ${binary} ]]; then
+  docker build -t $RELEASE_IMAGE --build-arg BINARY=$(basename ${binary}) --no-cache --rm -f ./Dockerfile .
+else
+  echo "unable to locate binary file '${binary}', stop"
+fi
 
 docker build -t $RELEASE_IMAGE ${ENV} --no-cache --rm -f ./Dockerfile .
